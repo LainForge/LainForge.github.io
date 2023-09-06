1bb006ca72f7a838dbe092fea00ec73d0d204ca8
Rohan Singh <rohansingh@Rohans-MacBook-Pro.local>
2023-09-06 13:12:01 +0530

    Added email flag in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Check if Certbot needs to obtain or renew the certificate
-certbot certonly --nginx --non-interactive --agree-tos -d lainforge.org -d www.lainforge.org
+certbot certonly --nginx --non-interactive --agree-tos --email "rohansinghagent@gmail.com" -d lainforge.org -d www.lainforge.org
 
 # Start Nginx
 nginx -g "daemon off;"
