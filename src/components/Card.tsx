06c72f349a0ec03963a54ec4b70dd50e7148382d
TarunAdobe <ttomar@adobe.com>
2023-09-15 13:16:01 +0530

    chore: added a blog about neura-launch

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -13,19 +13,19 @@ export default function Card({ href, frontmatter, secHeading = true }: Props) {
     <li className="my-6">
       <a
         href={href}
-        className="mb-1 inline-block text-lg font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
+        className="mb-1 inline-block text-xl font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
       >
         {secHeading ? (
-          <h2 className="text-lg font-medium decoration-dashed hover:underline">
+          <h2 className="text-xl font-medium decoration-dashed hover:underline">
             {title}
           </h2>
         ) : (
-          <h3 className="text-lg font-medium decoration-dashed hover:underline">
+          <h3 className="text-xl font-medium decoration-dashed hover:underline">
             {title}
           </h3>
         )}
       </a>
-      <Datetime datetime={pubDatetime} />
+      <Datetime datetime={pubDatetime} className="mt-1"/>
       <p className="mt-2">{description}</p>
     </li>
   );
