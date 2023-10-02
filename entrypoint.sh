aeb6a13d5f4fbb1f76015b2534c44134beda7eb7
Saahil Bhavsar <saahil_bhavsar@outlook.com>
2023-10-03 04:05:09 +0530

    Adds certbot container for SSL certificate

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-# Check if Certbot needs to obtain or renew the certificate
-certbot certonly --nginx --non-interactive --agree-tos --email "rohansinghagent@gmail.com" -d lainforge.org -d www.lainforge.org
-
-# Start Nginx
 nginx -g "daemon off;"
